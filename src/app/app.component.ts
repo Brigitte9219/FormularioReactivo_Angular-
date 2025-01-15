8d7a1604cb75ce07ae82db13823be3677ad5a205
BrigittePadilla <martha.padilla@croydon.com.co>
2025-01-14 21:20:18 -0500

    Creando el componente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ReactivoComponent } from "./reactivo/reactivo.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ReactivoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/reactivo/reactivo.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reactivo',
+  standalone: true,
+  imports: [],
+  templateUrl: './reactivo.component.html',
+  styleUrl: './reactivo.component.css'
+})
+export class ReactivoComponent {
+
+}
