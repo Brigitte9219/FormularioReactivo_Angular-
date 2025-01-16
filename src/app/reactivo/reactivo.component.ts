e521596262e11d55c9350a1b09f14ab27c6f5c61
BrigittePadilla <martha.padilla@croydon.com.co>
2025-01-15 21:05:36 -0500

    Otras validaciones ... uso de formGroup (formulario general) y formControlName (input)...

--- src/app/reactivo/reactivo.component.ts
+++ src/app/reactivo/reactivo.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactivo',
@@ -11,7 +11,25 @@ import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 })
 export class ReactivoComponent {
 
-  name = new FormControl('', Validators.required);
-  email = new FormControl('', [ Validators.required, Validators.email]);
+
+  get name(){
+    return this.formUser.get('name') as FormControl;
+  }
+
+  get email(){
+    return this.formUser.get('email') as FormControl;
+  }
+
+  formUser = new FormGroup({
+    'name': new FormControl('', Validators.required),
+    'email': new FormControl('', [ Validators.required, Validators.email])
+  });
+
+  //name = new FormControl('', Validators.required);
+  //email = new FormControl('', [ Validators.required, Validators.email]);
+
+  procesar(){
+    console.log(this.formUser.value);
+  }
 
 }
